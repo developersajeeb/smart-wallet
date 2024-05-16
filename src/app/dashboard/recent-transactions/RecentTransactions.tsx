04252b2876ed82e158df2bd4879cf6fbed2a83f4
developersajeeb <sajeebdeveloper@gmail.com>
2024-05-16 23:00:54 +0600

    fixed some ui

--- src/app/dashboard/recent-transactions/RecentTransactions.tsx
+++ src/app/dashboard/recent-transactions/RecentTransactions.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { GrTransaction } from 'react-icons/gr';
 
@@ -9,9 +10,56 @@ const RecentTransactions = () => {
                 <h4 className='text-gray-800 dark:text-white font-medium text-xl'>Recent Transactions</h4>
             </div>
 
-            <ul>
-                <li></li>
+            <ul className='mt-5 grid gap-3'>
+                <li className='flex justify-between bg-slate-50 dark:bg-dark-50 px-5 py-3 rounded-xl primary-shadow dark:shadow-md'>
+                    <div>
+                        <span className='text-xs font-medium text-primary-200'>May 27, 2024 @ 10:00 PM</span>
+                        <h3 className='font-medium text-gray-800 dark:text-white'>$60 Spent From Cash</h3>
+                        <span className='text-xs font-medium text-gray-500 dark:text-gray-400'>Account: Cash</span>
+                    </div>
+                    <div>
+                        <p className='text-xs text-gray-500 dark:text-gray-400'>Personal Spending</p>
+                        <span className='text-primary-200 text-lg font-semibold'>$60</span>
+                    </div>
+                </li>
+                <li className='flex justify-between bg-slate-50 dark:bg-dark-50 px-5 py-3 rounded-xl primary-shadow dark:shadow-md'>
+                    <div>
+                        <span className='text-xs font-medium text-primary-200'>May 27, 2024 @ 10:00 PM</span>
+                        <h3 className='font-medium text-gray-800 dark:text-white'>$60 Spent From Cash</h3>
+                        <span className='text-xs font-medium text-gray-500 dark:text-gray-400'>Account: Cash</span>
+                    </div>
+                    <div>
+                        <p className='text-xs text-gray-500 dark:text-gray-400'>Personal Spending</p>
+                        <span className='text-primary-200 text-lg font-semibold'>$60</span>
+                    </div>
+                </li>
+                <li className='flex justify-between bg-slate-50 dark:bg-dark-50 px-5 py-3 rounded-xl primary-shadow dark:shadow-md'>
+                    <div>
+                        <span className='text-xs font-medium text-primary-200'>May 27, 2024 @ 10:00 PM</span>
+                        <h3 className='font-medium text-gray-800 dark:text-white'>$60 Spent From Cash</h3>
+                        <span className='text-xs font-medium text-gray-500 dark:text-gray-400'>Account: Cash</span>
+                    </div>
+                    <div>
+                        <p className='text-xs text-gray-500 dark:text-gray-400'>Personal Spending</p>
+                        <span className='text-primary-200 text-lg font-semibold'>$60</span>
+                    </div>
+                </li>
+                <li className='flex justify-between bg-slate-50 dark:bg-dark-50 px-5 py-3 rounded-xl primary-shadow dark:shadow-md'>
+                    <div>
+                        <span className='text-xs font-medium text-primary-200'>May 27, 2024 @ 10:00 PM</span>
+                        <h3 className='font-medium text-gray-800 dark:text-white'>$60 Spent From Cash</h3>
+                        <span className='text-xs font-medium text-gray-500 dark:text-gray-400'>Account: Cash</span>
+                    </div>
+                    <div>
+                        <p className='text-xs text-gray-500 dark:text-gray-400'>Personal Spending</p>
+                        <span className='text-primary-200 text-lg font-semibold'>$60</span>
+                    </div>
+                </li>
             </ul>
+
+            <div className='mt-3 text-end'>
+                <Link href='/analytics' className='text-primary-300 font-medium cursor-pointer'>View All</Link>
+            </div>
         </div>
     );
 };
--- src/app/dashboard/total-balance/TotalBalance.tsx
+++ src/app/dashboard/total-balance/TotalBalance.tsx
@@ -9,6 +9,8 @@ import { InputNumber } from 'primereact/inputnumber';
 import { Button } from 'primereact/button';
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
+import { Calendar } from 'primereact/calendar';
+import { Nullable } from "primereact/ts-helpers";
 
 interface Account {
     name: string;
@@ -18,6 +20,8 @@ const TotalBalance = () => {
     const [visible, setVisible] = useState<boolean>(false);
     const [accountType, setAccountType] = useState<Account | null>(null);
     const [transactionTitle, setTransactionTitle] = useState<string>('');
+    const [amount, setAmount] = useState<number>();
+    const [date, setDate] = useState<Nullable<Date>>(null);
     const account: Account[] = [
         { name: 'Card' },
         { name: 'Bank Account' },
@@ -66,21 +70,27 @@ const TotalBalance = () => {
                 <p className='mt-1 text-sm text-end font-light text-gray-400'>Weekly analytics</p>
             </div>
             <Dialog header="Deposit" className='sw-dialog-box' visible={visible} onHide={() => setVisible(false)}>
-                <div>
-                    <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>To Account</label>
-                    <Dropdown value={accountType} onChange={(e: DropdownChangeEvent) => setAccountType(e.value)} options={account} optionLabel="name"
-                        editable placeholder="Select a City" className="w-full" />
-                </div>
-                <div className='mt-5'>
-                    <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>Transaction Title</label>
-                    <InputText className='w-full sw-input' value={transactionTitle} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTransactionTitle(e.target.value)} />
-                </div>
-                {/* <div className='mt-5'>
-                    <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>Present Amounts</label>
-                    <InputNumber className='sw-input w-full' inputId="target" value={nowValue} onValueChange={(e) => setNowValue(e.value ?? 0)} />
-                </div> */}
-                <div className='text-center mt-5'>
-                    <Button className='sw-button' label="Submit" />
+                <div className='grid sm:grid-cols-2 gap-5'>
+                    <div>
+                        <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>To Account</label>
+                        <Dropdown value={accountType} onChange={(e: DropdownChangeEvent) => setAccountType(e.value)} options={account} optionLabel="name"
+                            editable placeholder="Select a account" className="w-full" />
+                    </div>
+                    <div>
+                        <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>Transaction Title</label>
+                        <InputText className='w-full sw-input' value={transactionTitle} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTransactionTitle(e.target.value)} />
+                    </div>
+                    <div>
+                        <label className='block font-medium text-gray-600 dark:text-white mb-2 text-base' htmlFor='target'>Amounts</label>
+                        <InputNumber className='sw-input w-full' inputId="target" value={amount} onValueChange={(e) => setAmount(e.value ?? 0)} />
+                    </div>
+                    <div>
+                        <label className='block font-medium text-gray-600 dark:text-white text-base mb-2' htmlFor='target'>Date</label>
+                        <Calendar className='w-full' value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" />
+                    </div>
+                    <div className='text-center mt-5 sm:col-span-2'>
+                        <Button className='sw-button' label="Submit" />
+                    </div>
                 </div>
             </Dialog>
         </div>
