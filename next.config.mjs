64574d3f516f84457589940d6ccf78c90d47d79a
developersajeeb <sajeebdeveloper@gmail.com>
2024-05-26 00:35:34 +0600

    money card created

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
     webpack(config) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            fs: false,
-        };
-
-        // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test?.('.svg'));
-
-        config.module.rules.push({
-            oneOf: [
-                // Reapply the existing rule, but only for svg imports ending in ?url
-                {
-                    ...fileLoaderRule,
-                    test: /\.svg$/i,
-                    resourceQuery: /url/, // *.svg?url
-                },
-                // Convert all other *.svg imports to React components
-                {
-                    test: /\.svg$/i,
-                    issuer: /\.[jt]sx?$/,
-                    resourceQuery: { not: /url/ }, // exclude if *.svg?url
-                    use: ['@svgr/webpack'],
-                },
-            ],
-        });
-
-        // Modify the file loader rule to ignore *.svg, since we have it handled now.
-        fileLoaderRule.exclude = /\.svg$/i;
-
-        return config;
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
     },
-};
-
+  };
 export default nextConfig;
--- src/app/(user)/balance/page.tsx
+++ src/app/(user)/balance/page.tsx
@@ -3,30 +3,82 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
-// import MasterCardLogo from 'images/logos/mastercard_logo.svg?url';
+import MasterCardLogo from '/public/images/logos/mastercard_logo.svg';
+import VisaLogo from '/public/images/logos/visa_logo.svg';
+import CardMicroInfo from '/public/images/icons/card-helper.svg';
 
 const Balance = () => {
     return (
         <section className='px-4 pb-4 md:px-7 md:pb-7 grid grid-cols-3 gap-5'>
-            <div>
-                <div className='flex items-center justify-between'>
-                    <h4 className='text-gray-800 font-medium text-lg'>BRAC Bank</h4>
+            <div className='relative'>
+                <div className='flex items-center justify-between mb-2'>
+                    <h4 className='text-gray-800 dark:text-white font-medium text-lg'>BRAC Bank</h4>
                     <Link href='#' className='text-sm font-medium flex items-center text-primary-300 cursor-pointer'>
                         <span>View Details</span>
                         <span><IoIosArrowForward /></span>
                     </Link>
                 </div>
 
-                <div className='bg-secondary-300 p-6'>
+                <div className='bg-gradient-to-r  from-secondary-300 to-blue-500 p-6 rounded-3xl min-h-56 grid virtual-card'>
                     <div className='flex justify-between'>
                         <div>
-                            <h5>Current Balance</h5>
-                            <h2>$5750.20</h2>
+                            <h5 className='text-sm text-white opacity-70 tracking-wider'>Current Balance</h5>
+                            <h2 className='text-white text-2xl font-medium mt-2 word-break'>$5750.20</h2>
                         </div>
                         <div>
-                            {/* <Image className='w-[45]' src={MasterCardLogo} width={50} height={50} alt='Master Card' /> */}
-                            {/* <MasterCardLogo /> */}
+                            <MasterCardLogo />
                         </div>
+                    </div>
+
+                    <div className='flex justify-between items-end'>
+                        <div className='grid gap-2 align-bottom'>
+                            {/* <div className='flex flex-col justify-end'>
+                                <h5 className='text-sm text-white opacity-70 tracking-wider'>Account Number</h5>
+                                <p className='text-white text-base word-break'>43457323229896600</p>
+                            </div> */}
+                            <div className='flex flex-col justify-end'>
+                                <h5 className='text-sm text-white opacity-70 tracking-wider'>Card Number</h5>
+                                <p className='text-white text-base word-break'>013 4532 5463 297</p>
+                            </div>
+                        </div>
+
+                        <span><CardMicroInfo /></span>
+                    </div>
+                </div>
+            </div>
+            <div className='relative'>
+                <div className='flex items-center justify-between mb-2'>
+                    <h4 className='text-gray-800 dark:text-white font-medium text-lg'>DBBL Bank</h4>
+                    <Link href='#' className='text-sm font-medium flex items-center text-primary-300 cursor-pointer'>
+                        <span>View Details</span>
+                        <span><IoIosArrowForward /></span>
+                    </Link>
+                </div>
+
+                <div className='bg-gradient-to-r  from-primary-300 to-lime-500 p-6 rounded-3xl min-h-56 grid virtual-card'>
+                    <div className='flex justify-between'>
+                        <div>
+                            <h5 className='text-sm text-white opacity-70 tracking-wider'>Current Balance</h5>
+                            <h2 className='text-white text-2xl font-medium mt-2 word-break'>$5750.20</h2>
+                        </div>
+                        <div>
+                            <VisaLogo />
+                        </div>
+                    </div>
+
+                    <div className='flex justify-between items-end'>
+                        <div className='grid gap-2 align-bottom'>
+                            <div className='flex flex-col justify-end'>
+                                <h5 className='text-sm text-white opacity-70 tracking-wider'>Account Number</h5>
+                                <p className='text-white text-base word-break'>43457323229896600</p>
+                            </div>
+                            <div className='flex flex-col justify-end'>
+                                <h5 className='text-sm text-white opacity-70 tracking-wider'>Card Number</h5>
+                                <p className='text-white text-base word-break'>013 4532 5463 297</p>
+                            </div>
+                        </div>
+
+                        <span><CardMicroInfo /></span>
                     </div>
                 </div>
             </div>
